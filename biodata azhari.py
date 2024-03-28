ceb00e604fd06e886b24c49617e24ed3f25f9921
azzzhari <165252453+azzzhari@users.noreply.github.com>
2024-03-28 15:32:59 +0800

    Add files via upload

---
+++ biodata azhari.py
@@ -0,0 +1,18 @@
+#buat program untuk menampilkan biodata
+
+
+nama=("Azhari")
+nim=("SI20230006")
+semester=("2")
+umur=int(25)
+alamat=("Praya")
+lulusan=("Paket C")
+
+print("=="*10)
+print("selamat datang", nama)
+print("nim anda", nim)
+print("anda semester",semester)
+print("umur anda", umur)
+print("alamat anda", alamat)
+print("lulusan sekolah", lulusan)
+print("=="*10)
